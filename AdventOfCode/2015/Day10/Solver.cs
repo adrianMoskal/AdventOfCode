ed5e0a908ec4eda061578af6149ee505b277320f
adrianMoskal <adrian.moskal@icloud.com>
2022-02-10 14:44:27 +0100

    Add 2015 Day10 solutions

---
+++ AdventOfCode/2015/Day10/Solver.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode._2015.Day10
+{
+    class Solver : ISolver
+    {
+        private const string Path = @"../../../2015/Day10/puzzleInput.txt";
+
+        public void PartOne()
+        {
+            string input = File.ReadAllText(Path);
+
+            int solution = LookAndSayNumbers(input).ElementAt(39).Length;
+
+            Console.WriteLine($"Part One: {solution} ");
+        }
+
+        public void PartTwo()
+        {
+            string input = File.ReadAllText(Path);
+
+            int solution = LookAndSayNumbers(input).ElementAt(49).Length;
+
+            Console.WriteLine($"Part Two: {solution} ");
+        }
+
+        IEnumerable<string> LookAndSayNumbers(string number)
+        {
+            for(int i = 0; i < int.MaxValue; i++)
+            {
+                number = LookAndSay(number);
+                yield return number;
+            }
+        }
+
+
+        private string LookAndSay(string input)
+        {
+            StringBuilder output = new StringBuilder();
+            int i = 0;
+            while (i < input.Length)
+            {
+                int count = 1;
+                while ((i + 1 < input.Length) && (input[i] == input[i + 1]))
+                {
+                    count++;
+                    i++;
+                }
+
+                output.Append(count.ToString() + input[i]);
+                i++;
+            }
+            return output.ToString();
+        }
+    }
+}
