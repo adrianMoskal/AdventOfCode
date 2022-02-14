ea5be7466c01d384f7a5ab0acb23343ab9911271
adrianMoskal <adrian.moskal@icloud.com>
2022-02-14 09:25:22 +0100

    Add 2015 Day16 solutions

---
+++ AdventOfCode/2015/Day16/Aunt.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode._2015.Day16
+{
+    class Aunt : IEquatable<Aunt>
+    {
+        public int Number { get; set; }
+        public int? Children { get; set; }
+        public int? Cats { get; set; }
+        public int? Samoyeds { get; set; }
+        public int? Pomeranians { get; set; }
+        public int? Akitas { get; set; }
+        public int? Vizslas { get; set; }
+        public int? Goldfish { get; set; }
+        public int? Trees { get; set; }
+        public int? Cars { get; set; }
+        public int? Perfumes { get; set; }
+        
+        public Aunt(int number)
+        {
+            Number = number;
+        }
+
+        public bool Equals(Aunt other)
+        {
+            if (other.Children.HasValue && Children != other.Children)
+                return false;
+            if (other.Cats.HasValue && Cats != other.Cats)
+                return false;
+            if (other.Samoyeds.HasValue && Samoyeds != other.Samoyeds)
+                return false;
+            if (other.Pomeranians.HasValue && Pomeranians != other.Pomeranians)
+                return false;
+            if (other.Akitas.HasValue && Akitas != other.Akitas)
+                return false;
+            if (other.Vizslas.HasValue && Vizslas != other.Vizslas)
+                return false;
+            if (other.Goldfish.HasValue && Goldfish != other.Goldfish)
+                return false;
+            if (other.Trees.HasValue && Trees != other.Trees)
+                return false;
+            if (other.Cars.HasValue && Cars != other.Cars)
+                return false;
+            if (other.Perfumes.HasValue && Perfumes != other.Perfumes)
+                return false;
+
+            return true;
+        }
+
+        public bool EqualsPartTwo(Aunt other)
+        {
+            if (other.Children.HasValue && Children != other.Children)
+                return false;
+            if (other.Cats.HasValue && Cats >= other.Cats)
+                return false;
+            if (other.Samoyeds.HasValue && Samoyeds != other.Samoyeds)
+                return false;
+            if (other.Pomeranians.HasValue && Pomeranians <= other.Pomeranians)
+                return false;
+            if (other.Akitas.HasValue && Akitas != other.Akitas)
+                return false;
+            if (other.Vizslas.HasValue && Vizslas != other.Vizslas)
+                return false;
+            if (other.Goldfish.HasValue && Goldfish <= other.Goldfish)
+                return false;
+            if (other.Trees.HasValue && Trees >= other.Trees)
+                return false;
+            if (other.Cars.HasValue && Cars != other.Cars)
+                return false;
+            if (other.Perfumes.HasValue && Perfumes != other.Perfumes)
+                return false;
+
+            return true;
+        }
+    }
+}
---
+++ AdventOfCode/2015/Day16/Solver.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode._2015.Day16
+{
+    class Solver : ISolver
+    {
+        private const string Path = @"../../../2015/Day16/puzzleInput.txt";
+
+        private Aunt pattern = new Aunt(-1)
+        {
+            Children = 3,
+            Cats = 7,
+            Samoyeds = 2,
+            Pomeranians = 3,
+            Akitas = 0,
+            Vizslas = 0,
+            Goldfish = 5,
+            Trees = 3,
+            Cars = 2,
+            Perfumes = 1
+        };
+
+        public void PartOne()
+        {
+            string[] lines = File.ReadAllLines(Path);
+            List<Aunt> aunts = Parse(lines);
+
+            int solution = aunts.Single(a => pattern.Equals(a)).Number;
+
+            Console.WriteLine($"Part One: {solution}");
+        }
+
+        public void PartTwo()
+        {
+            string[] lines = File.ReadAllLines(Path);
+            List<Aunt> aunts = Parse(lines);
+
+            int solution = aunts.Single(a => pattern.EqualsPartTwo(a)).Number;
+
+            Console.WriteLine($"Part Two: {solution}");
+        }
+
+        private List<Aunt> Parse(string[] input)
+        {
+            var aunts = new List<Aunt>();
+
+            int number = 1;
+            foreach(var line in input)
+            {
+                var aunt = new Aunt(number++);
+                var elements = line.Replace(",", "").Split(" ");
+                for(int i = 0; i < elements.Length; i++)
+                {
+                    switch(elements[i])
+                    {
+                        case "children:":
+                            aunt.Children = int.Parse(elements[i + 1]);
+                            break;
+                        case "cats:":
+                            aunt.Cats = int.Parse(elements[i + 1]);
+                            break;
+                        case "samoyeds:":
+                            aunt.Samoyeds = int.Parse(elements[i + 1]);
+                            break;
+                        case "pomeranians:":
+                            aunt.Pomeranians = int.Parse(elements[i + 1]);
+                            break;
+                        case "akitas:":
+                            aunt.Akitas = int.Parse(elements[i + 1]);
+                            break;
+                        case "vizslas:":
+                            aunt.Vizslas = int.Parse(elements[i + 1]);
+                            break;
+                        case "goldfish:":
+                            aunt.Goldfish = int.Parse(elements[i + 1]);
+                            break;
+                        case "trees:":
+                            aunt.Trees = int.Parse(elements[i + 1]);
+                            break;
+                        case "cars:":
+                            aunt.Cars = int.Parse(elements[i + 1]);
+                            break;
+                        case "perfumes:":
+                            aunt.Perfumes = int.Parse(elements[i + 1]);
+                            break;
+                    }
+                }
+                aunts.Add(aunt);
+            }
+
+            return aunts;
+        }
+    }
+}
