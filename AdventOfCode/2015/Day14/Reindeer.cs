3a7f218ec3f5a73595409b006e215bca0490211c
adrianMoskal <adrian.moskal@icloud.com>
2022-02-14 08:19:37 +0100

    Add 2015 Day14 solutions

---
+++ AdventOfCode/2015/Day14/Reindeer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode._2015.Day14
+{
+    class Reindeer
+    {
+        public string Name { get; set; }
+        public int Speed { get; set; }
+        public int FlyTime { get; set; }
+        public int TimeSpentFlying { get; set; }
+        public int RestTime { get; set; }
+        public int TimeSpentResting { get; set; }
+        public bool IsFlying { get; set; }
+        public int Distance { get; set; }
+        public int Points { get; set; }
+
+        public Reindeer(string name, int speed, int flyTime, int restTime, bool isFlying)
+        {
+            Name = name;
+            Speed = speed;
+            FlyTime = flyTime;
+            RestTime = restTime;
+            IsFlying = isFlying;
+            Points = TimeSpentFlying = TimeSpentResting = Distance = 0;
+        }
+
+        public void Step()
+        {
+            if(IsFlying)
+            {
+                Distance += Speed;
+                TimeSpentFlying++;
+                if(TimeSpentFlying >= FlyTime)
+                {
+                    TimeSpentFlying = 0;
+                    IsFlying = false;
+                }
+            }
+            else
+            {
+                TimeSpentResting++;
+                if (TimeSpentResting >= RestTime)
+                {
+                    TimeSpentResting = 0;
+                    IsFlying = true;
+                }
+            }
+        }
+    }
+}
---
+++ AdventOfCode/2015/Day14/Solver.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode._2015.Day14
+{
+    class Solver : ISolver
+    {
+        private const string Path = @"../../../2015/Day14/puzzleInput.txt";
+
+        public void PartOne()
+        {
+            string[] lines = File.ReadAllLines(Path);
+            int seconds = 2503;
+
+            var reindeers = Parse(lines);
+
+            for (int i = 0; i < seconds; i++)
+                reindeers.ForEach(r => r.Step());
+
+            int solution = reindeers.Max(r => r.Distance);
+
+            Console.WriteLine($"Part One: {solution}");
+        }
+
+        public void PartTwo()
+        {
+            string[] lines = File.ReadAllLines(Path);
+            int seconds = 2503;
+
+            var reindeers = Parse(lines);
+
+            for (int i = 0; i < seconds; i++)
+            {
+                reindeers.ForEach(r => r.Step());
+                reindeers.Where(r => r.Distance == reindeers.Max(x => x.Distance)).ToList().ForEach(r => r.Points++);
+            }   
+
+            int solution = reindeers.Max(r => r.Points);
+
+            Console.WriteLine($"Part Two: {solution}");
+        }
+
+        private List<Reindeer> Parse(string[] input)
+        {
+            var reindeers = new List<Reindeer>();
+
+            foreach (var line in input)
+            {
+                var elements = line.Split(" ");
+                reindeers.Add(new Reindeer(elements[0], int.Parse(elements[3]), int.Parse(elements[6]), int.Parse(elements[13]), true));
+            }
+
+            return reindeers;
+        }
+    }
+}
