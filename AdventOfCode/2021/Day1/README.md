7c42f61456e5f19394551df62003113490171295
adrianMoskal <adrian.moskal@icloud.com>
2022-02-25 12:33:08 +0100

    Add 2021 Day2 solutions

--- AdventOfCode/2021/Day1/README.md
+++ AdventOfCode/2021/Day1/README.md
@@ -1,4 +1,4 @@
-﻿SOURCE: https://adventofcode.com/2019/day/1
+﻿SOURCE: https://adventofcode.com/2021/day/1
 
 # --- Day 1: Sonar Sweep ---
 You're minding your own business on a ship at sea when the overboard alarm goes off! You rush to see if you can help. Apparently, one of the Elves tripped and accidentally sent the sleigh keys flying into the ocean!
---
+++ AdventOfCode/2021/Day2/Solver.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode._2021.Day2
+{
+    class Solver : ISolver
+    {
+        private const string Path = @"../../../2021/Day2/puzzleInput.txt";
+        public void PartOne()
+        {
+            string[] lines = File.ReadAllLines(Path);
+
+            var state = Dive(lines, false).Last();
+            int solution = state[0] * state[1];
+
+            Console.WriteLine($"Part One: {solution}");
+        }
+
+        public void PartTwo()
+        {
+            string[] lines = File.ReadAllLines(Path);
+
+            var state = Dive(lines, true).Last();
+            int solution = state[0] * state[1];
+
+            Console.WriteLine($"Part Two: {solution}");
+        }
+
+        IEnumerable<List<int>> Dive(string[] instructions, bool aiming)
+        {
+            var state = new List<int>() { 0, 0, 0 };
+            yield return state;
+
+            if (aiming)
+            {
+                foreach (var instruction in instructions)
+                {
+                    string command = instruction.Split(" ")[0];
+                    int value = int.Parse(instruction.Split(" ")[1]);
+
+                    state = new List<int>(state);
+                    switch (command)
+                    {
+                        case "forward":
+                            state[0] += value;
+                            state[1] += state[2] * value;
+                            break;
+                        case "down":
+                            state[2] += value;
+                            break;
+                        case "up":
+                            state[2] -= value;
+                            break;
+                    }
+                    yield return state;
+                }
+            }
+            else
+            {
+                foreach (var instruction in instructions)
+                {
+                    string command = instruction.Split(" ")[0];
+                    int value = int.Parse(instruction.Split(" ")[1]);
+
+                    state = new List<int>(state);
+                    switch (command)
+                    {
+                        case "forward":
+                            state[0] += value;
+                            break;
+                        case "down":
+                            state[1] += value;
+                            break;
+                        case "up":
+                            state[1] -= value;
+                            break;
+                    }
+                    yield return state;
+                }
+            }
+            
+        }
+    }
+}
