89c2caae387d1cae07020cc08110ed2ce7d9f57d
adrianMoskal <adrian.moskal@icloud.com>
2022-02-25 11:30:55 +0100

    Add 2021 Day1 solutions

---
+++ AdventOfCode/2021/Day1/Solver.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode._2021.Day1
+{
+    class Solver : ISolver
+    {
+        private const string Path = @"../../../2021/Day1/puzzleInput.txt";
+
+        public void PartOne()
+        {
+            string[] lines = File.ReadAllLines(Path);
+            var measurements = lines.Select(Int32.Parse);
+
+            int solution = Enumerable.Range(1, lines.Length - 1)
+                .Count(x => measurements.ElementAt(x) > measurements.ElementAt(x - 1));
+
+            Console.WriteLine($"Part One: {solution}");
+        }
+
+        public void PartTwo()
+        {
+            string[] lines = File.ReadAllLines(Path);
+            var measurements = lines.Select(Int32.Parse);
+
+            var windowSums = Enumerable.Range(0, lines.Length - 2)
+                .Select(x => measurements.ElementAt(x) + measurements.ElementAt(x + 1) + measurements.ElementAt(x + 2));
+
+            int solution = Enumerable.Range(1, windowSums.Count() - 1).Count(x => windowSums.ElementAt(x) > windowSums.ElementAt(x - 1));
+
+            Console.WriteLine($"Part Two: {solution}");
+        }
+    }
+}
