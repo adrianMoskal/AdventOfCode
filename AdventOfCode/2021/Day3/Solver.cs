0dcec1af5c6edf2b64c2099609bbb370dde95e1e
adrianMoskal <adrian.moskal@icloud.com>
2022-02-28 15:23:44 +0100

    Add 2021 Day3 Part Two solution

--- AdventOfCode/2021/Day3/Solver.cs
+++ AdventOfCode/2021/Day3/Solver.cs
@@ -13,7 +13,7 @@ namespace AdventOfCode._2021.Day3
 
         public void PartOne()
         {
-            string[] bytes = File.ReadAllLines(Path);
+            var bytes = File.ReadAllLines(Path).ToList();
             
             var gammaRateBinary = string.Join("", Enumerable.Range(0, bytes[0].Length).Select(x => MostCommonBit(bytes, x)));
             var epsilonRateBinary = string.Join("", gammaRateBinary.Select(b => b == '0' ? '1' : '0'));
@@ -28,18 +28,38 @@ namespace AdventOfCode._2021.Day3
 
         public void PartTwo()
         {
-            Console.WriteLine("No Part Two yet");
+            string[] bytes = File.ReadAllLines(Path);
+
+            int i = 0;
+            var bytesForOxygen = new List<string>(bytes);
+            while(bytesForOxygen.Count() > 1)
+            {
+                bytesForOxygen = bytesForOxygen.Where(b => b[i].Equals(MostCommonBit(bytesForOxygen, i))).ToList();
+                i++;
+            }
+
+            i = 0;
+            var bytesForCO2 = new List<string>(bytes);
+            while (bytesForCO2.Count() > 1)
+            {
+                bytesForCO2 = bytesForCO2.Where(b => !b[i].Equals(MostCommonBit(bytesForCO2, i))).ToList();
+                i++;
+            }
+
+            int oxygenGeneratorRating = Convert.ToInt32(bytesForOxygen[0], 2);
+            int co2ScrubberRating = Convert.ToInt32(bytesForCO2[0], 2);
+
+            int solution = oxygenGeneratorRating * co2ScrubberRating;
+
+            Console.WriteLine($"Part Two: {solution}");
         }
 
-        private char MostCommonBit(string[] bytes, int position)
+        private char MostCommonBit(List<string> bytes, int position)
         {
-            return bytes
-               .Select(x => x[position])
-               .GroupBy(x => x)
-               .OrderByDescending(x => x.Count())
-               .Take(1)
-               .Select(x => x.Key)
-               .ElementAt(0);
+            int ones = bytes.Count(b => b.ElementAt(position).Equals('1'));
+            int zeroes = bytes.Count(b => b.ElementAt(position).Equals('0'));
+
+            return zeroes > ones ? '0' : '1';
         }
     }
 }
