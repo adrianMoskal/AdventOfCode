7c8403bf541aad586d99e6fe56ae6e35f38b4481
adrianMoskal <adrian.moskal@icloud.com>
2022-05-25 16:44:29 +0200

    Add 2016 Day4 Part Two solution

--- AdventOfCode/2016/Day4/Solver.cs
+++ AdventOfCode/2016/Day4/Solver.cs
@@ -6,30 +6,69 @@ internal sealed class Solver : ISolver
     {
         string[] lines = File.ReadAllLines(path);
 
-        int solution = 0;
+        var realRooms = RemoveDecoyRooms(lines);
 
-        foreach(var line in lines)
-        {
-            var checksum = line.Split('[', ']')[1];
-            var name = line.Substring(0, line.LastIndexOf('-'));
-            var sector = line.Substring(line.LastIndexOf('-')+1, line.IndexOf('[') - line.LastIndexOf('-') - 1);
-            
-            var mostCommon = name.Replace("-", "")
-                .OrderBy(x => x)
-                .GroupBy(x => x)
-                .OrderByDescending(x => x.Count())
-                .Take(5)
-                .Select(x => x.Key);
-
-            if (mostCommon.All(x => checksum.Contains(x)))
-                solution += int.Parse(sector);               
-        }
+        int solution = realRooms.Select(x => GetSector(x))
+            .Sum();
 
         Console.WriteLine($"Part One: {solution}");
     }
 
     public void PartTwo(string path)
     {
-        Console.WriteLine($"No Part Two yet");
+        string[] lines = File.ReadAllLines(path);
+
+        var realRooms = RemoveDecoyRooms(lines);
+
+        int solution = 0;
+
+        foreach(var room in realRooms)
+        {
+            var sector = GetSector(room);
+            var name = GetName(room);
+
+            var decoded = DecodeCipher(name, sector);
+
+            if (decoded.Contains("northpole"))
+            {
+                solution = sector;
+                break;
+            }
+        }
+
+        Console.WriteLine($"Part Two: {solution}");
+    }
+
+    private string GetName(string room)
+        => room.Substring(0, room.LastIndexOf('-')).Replace("-", "");
+
+    private int GetSector(string room)
+        => int.Parse(room.Substring(room.LastIndexOf('-') + 1, room.IndexOf('[') - room.LastIndexOf('-') - 1));
+
+    private string GetChecksum(string room)
+        => room.Split('[', ']')[1];
+
+    private string DecodeCipher(string txt, int shift)
+        => string.Concat(txt.Select(x => (char)((((x + shift) - 'a') % 26) + 'a')));
+
+    private IEnumerable<string> RemoveDecoyRooms(IEnumerable<string> rooms)
+    {
+        var realRooms = new List<string>();
+        rooms.ToList().ForEach(room =>
+        {
+            var checksum = GetChecksum(room);
+            var name = GetName(room);
+            
+            var mostCommon = name.OrderBy(x => x)
+                .GroupBy(x => x)
+                .OrderByDescending(x => x.Count())
+                .Take(5)
+                .Select(x => x.Key);
+
+            if (mostCommon.All(x => checksum.Contains(x)))
+                realRooms.Add(room);
+        });
+
+        return realRooms;
     }
 }
