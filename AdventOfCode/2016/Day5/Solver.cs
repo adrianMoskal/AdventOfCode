73f32a988155c7a7b7c8cdab6f075d1ad8012bc5
adrianMoskal <adrian.moskal@icloud.com>
2022-05-26 19:27:11 +0200

    Add 2016 Day 5 Part One solution

---
+++ AdventOfCode/2016/Day5/Solver.cs
@@ -0,0 +1,40 @@
+﻿namespace AdventOfCode._2016.Day5;
+
+internal sealed class Solver : ISolver
+{
+    public void PartOne(string path)
+    {
+        string input = File.ReadAllText(path);
+
+        int i = 0;
+        var solution = new StringBuilder();
+
+        while (solution.Length < 8)
+        {
+            var hash = "";
+
+            while (!hash.StartsWith("00000"))
+                hash = CreateMD5(input + i++);
+
+            solution.Append(hash[5]);
+        }
+
+        Console.WriteLine($"Part One: {solution}");
+    }
+
+    public void PartTwo(string path)
+    {
+        Console.WriteLine($"No Part Two yet");
+    }
+
+    public static string CreateMD5(string input)
+    {
+        using (MD5 md5 = MD5.Create())
+        {
+            byte[] inputBytes = Encoding.ASCII.GetBytes(input);
+            byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+            return Convert.ToHexString(hashBytes);
+        }
+    }
+}
