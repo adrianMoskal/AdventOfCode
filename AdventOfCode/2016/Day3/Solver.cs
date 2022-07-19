7103032c08a7fa1006adb12769118569423dd9a0
adrianMoskal <adrian.moskal@icloud.com>
2022-07-19 17:20:56 +0200

    Add 2016 Day3 Part Two solution

--- AdventOfCode/2016/Day3/Solver.cs
+++ AdventOfCode/2016/Day3/Solver.cs
@@ -6,22 +6,61 @@ internal sealed class Solver : ISolver
     {
         string[] lines = File.ReadAllLines(path);
 
-        var triangles = lines.Select(l => Regex.Matches(l, @"\d+").Select(x => int.Parse(x.Value)));
-
-        int solution = triangles.Count(t => ValidTriangle(t));
+        int solution = Parse(lines)
+            .Count(t => ValidTriangle(t));
 
         Console.WriteLine($"Part One: {solution}");
     }
 
     public void PartTwo(string path)
     {
-        Console.WriteLine($"No Part Two yet");
+        string[] lines = File.ReadAllLines(path);
+
+        var solution = GetTransposedTriangles(Parse(lines))
+            .Count(t => ValidTriangle(t));
+
+        Console.WriteLine($"Part Two: {solution}");
     }
+
+    private IEnumerable<IEnumerable<int>> Parse(IEnumerable<string> input)
+        => input.Select(l => Regex.Matches(l, @"\d+")
+                .Select(x => int.Parse(x.Value)));
 
     private bool ValidTriangle(IEnumerable<int> triangle)
     {
         return triangle.ElementAt(0) + triangle.ElementAt(1) > triangle.ElementAt(2)
             && triangle.ElementAt(0) + triangle.ElementAt(2) > triangle.ElementAt(1)
             && triangle.ElementAt(1) + triangle.ElementAt(2) > triangle.ElementAt(0);
+    }
+
+    private IEnumerable<IEnumerable<int>> GetTransposedTriangles(IEnumerable<IEnumerable<int>> lines)
+    {
+        var transposedMatrix = Transpose(lines);
+        var transposedTriangles = new List<List<int>>();
+
+        transposedMatrix.ToList().ForEach(t =>
+        {
+            for (int i = 0; i < t.Count() - 2; i += 3)
+                transposedTriangles.Add(t.Skip(i).Take(3).ToList());
+        });
+
+        return transposedTriangles;
+    }
+
+    private IEnumerable<IEnumerable<int>> Transpose(IEnumerable<IEnumerable<int>> matrix)
+    {
+        var transposed = new List<List<int>>();
+
+        for (int i = 0; i < matrix.First().Count(); i++)
+        {
+            var row = new List<int>();
+
+            for (int j = 0; j < matrix.Count(); j++)
+                row.Add(matrix.ElementAt(j).ElementAt(i));
+
+            transposed.Add(row);
+        }
+
+        return transposed;
     }
 }
