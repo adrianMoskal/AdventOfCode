50aa6de6872c79350b2b1547e8a289b723555166
adrianMoskal <adrian.moskal@icloud.com>
2022-07-20 01:41:58 +0200

    Add 2016 Day6 solutions

---
+++ AdventOfCode/2016/Day6/Solver.cs
@@ -0,0 +1,56 @@
+﻿namespace AdventOfCode._2016.Day6;
+
+internal sealed class Solver : ISolver
+{
+    public void PartOne(string path)
+    {
+        string[] lines = File.ReadAllLines(path);
+
+        var mostFrequents = Transpose(lines)
+            .Select(x => x
+                .GroupBy(x => x)
+                .OrderByDescending(x => x.Count())
+                .Take(1)
+                .Select(x => x.Key))
+            .Select(x => x.First());
+
+        var solution = string.Concat(mostFrequents);
+
+        Console.WriteLine($"Part One: {solution}");
+    }
+
+    public void PartTwo(string path)
+    {
+        string[] lines = File.ReadAllLines(path);
+
+        var mostFrequents = Transpose(lines)
+            .Select(x => x
+                .GroupBy(x => x)
+                .OrderBy(x => x.Count())
+                .Take(1)
+                .Select(x => x.Key))
+            .Select(x => x.First());
+
+        var solution = string.Concat(mostFrequents);
+
+        Console.WriteLine($"Part Two: {solution}");
+    }
+
+    private IEnumerable<string> Transpose(string[] matrix)
+    {
+        var transposed = new List<string>();
+
+        for (int i = 0; i < matrix.First().Count(); i++)
+        {
+            string row = "";
+
+            for (int j = 0; j < matrix.Count(); j++)
+                row += matrix[j][i];
+
+            transposed.Add(row);
+        }
+
+        return transposed;
+    }
+}
+
